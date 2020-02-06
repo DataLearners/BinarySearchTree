2ff2341db0a8d1ae63075fe9348e1d254e0cb9bb
RaiderDBrown <60452412+RaiderDBrown@users.noreply.github.com>
2020-02-05 23:10:27 -0500

    Final push for day
    no sig change

--- Diabetes_DecisionTree/MainScript.py
+++ Diabetes_DecisionTree/MainScript.py
@@ -139,51 +139,51 @@ ray_tree.leaf_matrix.export("Diabetes_LeafMatrix.csv", folder=outputpath)
 ray_tree.tree_array.export("Diabetes_Tree.csv", folder=outputpath)
 filename= "Diabetes_MarkedTrainingData.csv"
 ray_tree.marked_data.export(filename, folder=outputpath)
-
-y = df.header.index('BMI')
-bmi_tree = bst.Tree(train_data, y, df.header, loss='var', min_rows=15)
-filename = "BMI_Predictions.csv"
-bmi_tree.predict(test_data, conf=0.8).export(filename, folder=outputpath)
-leaf = random.sample(bmi_tree.leaves, 1)[0]
-bmi_tree.traverse_to(leaf)
-bmi_tree.leaf_matrix.export("BMI_LeafMatrix.csv", folder=outputpath)
-bmi_tree.tree_array.export("BMI_Tree.csv", folder=outputpath)
-bmi_tree.marked_data.export("BMI_MarkedTrainingData.csv", folder=outputpath)
-
-rows = 0
-horiz, mse, rsq, ftrs = [], [], [], []
-y = df.header.index('BMI')
-for i in range(5):
-    horiz.append(rows)
-    rows += 5
-    bmi_tree = bst.Tree(train_data, y, df.header, loss='var', min_rows=rows)
-    print("Min Rows {}".format(rows))
-    pred = bmi_tree.predict(test_data, conf=0.9)
-    mse.append(bmi_tree.mse)
-    rsq.append(bmi_tree.r_sq)
-    ftrs.append(len(bmi_tree.feature_importance))
-  
-plt.figure(figsize=(18, 6))
-plt.subplot(1,3,1)    
-x = np.asarray(horiz)
-plt.plot(x, np.asarray(mse))
-plt.axvline(x=50, ls=':')
-plt.ylabel('MSE')
-plt.xlabel('Minimum Node Size')
-plt.subplot(1,3,2)    
-x = np.asarray(horiz)
-plt.plot(x, np.asarray(rsq))
-plt.axvline(x=50, ls=':')
-plt.ylabel('RSquare')
-plt.xlabel('Minimum Node Size')
-plt.subplot(1,3,3)    
-x = np.asarray(horiz)
-plt.plot(x, np.asarray(ftrs))
-plt.axvline(x=50, ls=':')
-plt.ylabel('Features')
-plt.xlabel('Minimum Node Size')
-plt.savefig('Output\\AccuracyPlots.png')
-plt.show()
-
-bst.config.TIMES.plot_times()
+#
+#y = df.header.index('BMI')
+#bmi_tree = bst.Tree(train_data, y, df.header, loss='var', min_rows=15)
+#filename = "BMI_Predictions.csv"
+#bmi_tree.predict(test_data, conf=0.8).export(filename, folder=outputpath)
+#leaf = random.sample(bmi_tree.leaves, 1)[0]
+#bmi_tree.traverse_to(leaf)
+#bmi_tree.leaf_matrix.export("BMI_LeafMatrix.csv", folder=outputpath)
+#bmi_tree.tree_array.export("BMI_Tree.csv", folder=outputpath)
+#bmi_tree.marked_data.export("BMI_MarkedTrainingData.csv", folder=outputpath)
+#
+#rows = 0
+#horiz, mse, rsq, ftrs = [], [], [], []
+#y = df.header.index('BMI')
+#for i in range(5):
+#    horiz.append(rows)
+#    rows += 5
+#    bmi_tree = bst.Tree(train_data, y, df.header, loss='var', min_rows=rows)
+#    print("Min Rows {}".format(rows))
+#    pred = bmi_tree.predict(test_data, conf=0.9)
+#    mse.append(bmi_tree.mse)
+#    rsq.append(bmi_tree.r_sq)
+#    ftrs.append(len(bmi_tree.feature_importance))
+#  
+#plt.figure(figsize=(18, 6))
+#plt.subplot(1,3,1)    
+#x = np.asarray(horiz)
+#plt.plot(x, np.asarray(mse))
+#plt.axvline(x=50, ls=':')
+#plt.ylabel('MSE')
+#plt.xlabel('Minimum Node Size')
+#plt.subplot(1,3,2)    
+#x = np.asarray(horiz)
+#plt.plot(x, np.asarray(rsq))
+#plt.axvline(x=50, ls=':')
+#plt.ylabel('RSquare')
+#plt.xlabel('Minimum Node Size')
+#plt.subplot(1,3,3)    
+#x = np.asarray(horiz)
+#plt.plot(x, np.asarray(ftrs))
+#plt.axvline(x=50, ls=':')
+#plt.ylabel('Features')
+#plt.xlabel('Minimum Node Size')
+#plt.savefig('Output\\AccuracyPlots.png')
+#plt.show()
+#
+#bst.config.TIMES.plot_times()
 
